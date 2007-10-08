58b1c1172e0fd272f9640ce7dacad459e9fdee70
bcarter <bcarter>
2007-10-08 15:20:44 +0000

    Initial checkin of the top page of the project website.

--- index.php
+++ index.php
@@ -1,42 +1,135 @@
-<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head><title>Accessibility Tools Framework (ACTF)</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="author" content="Arro Walter" />
+<meta name="keywords" content="actf, ACTF, Javaco, Webelo, AccProbe, Accessibility, HA&AC" />
+<link rel="stylesheet" type="text/css" href="http://www.eclipse.org/eclipse.org-common/themes/Lazarus/css/small.css" title="small" />
+<link rel="alternate stylesheet" type="text/css" href="http://www.eclipse.org/eclipse.org-common/themes/Lazarus/css/large.css" title="large" />
+<link rel="stylesheet" type="text/css" href="http://www.eclipse.org/eclipse.org-common/themes/Lazarus/css/visual.css" media="screen" />
+<link rel="stylesheet" type="text/css" href="http://www.eclipse.org/eclipse.org-common/themes/Lazarus/css/layout.css" media="screen" />
+<link rel="stylesheet" type="text/css" href="http://www.eclipse.org/eclipse.org-common/themes/Phoenix/css/print.css" media="print" />
+<script type="text/javascript" src="http://www.eclipse.org/eclipse.org-common/themes/Phoenix/styleswitcher.js"></script>
+</head>
 
-	#*****************************************************************************
-	#
-	# template.php
-	#
-	# Author: 		Denis Roy
-	# Date:			2005-06-16
-	#
-	# Description: Type your page comments here - these are not sent to the browser
-	#
-	#
-	#****************************************************************************
+<body>
+<div id="header">
+</div>
+
+<div id="headerlogo">
+   <a href="/"><img src="http://www.eclipse.org/eclipse.org-common/themes/Lazarus/images/header_logo.gif" 
+    alt="Eclipse Logo" class="logo" border="0" height="75" width="241" /></a>
+</div>
+
+<div id="topnav">
+	<ul>
+	<li><a class="" href="#" target="_self">Home</a></li>
+	<li><a class="" href="#" target="_self">Community</a></li>
+	<li><a class="" href="#" target="_self">Membership</a></li>
+	<li><a class="" href="#" target="_self">Committers</a></li>
+	<li><a class="" href="#" target="_self">Downloads</a></li>
+	<li><a class="" href="#" target="_self">Resources</a></li>
+	<li><a class="" href="#" target="_self">Projects</a></li>
+	<li><a class="" href="#" target="_self">About Us</a></li>
+	</ul>
+</div>
+
+<div id="searchbar">
+   <form method="get" action="/search/search.cgi">Search:
+      <input type="hidden" name="t" value="All" />
+      <input type="hidden" name="t" value="Doc" />
+      <input type="hidden" name="t" value="Downloads" />
+      <input type="hidden" name="t" value="Wiki" />
+      <input type="hidden" name="t" value="Live" />
+      <input type="hidden" name="wf" value="574a74" />
+      <input type="text" class="textfield" name="q" value="" />
+      <input type="image" class="button" src="http://www.eclipse.org/eclipse.org-common/themes/Phoenix/images/searchbar_submit.gif" 
+                   alt="Submit" onclick="this.submit();" />
+   </form>	
+</div>
+
+<div id="topnavsep">
+</div>
+
+<div id="leftcol">
+   <ul id="leftnav">
+      <li class="separator"><a class="separator" href="index.htm">ACTF &#160;&#160;
+                            <img src="http://www.eclipse.org/eclipse.org-common/themes/Phoenix/images/leftnav_bullet_down.gif" border="0" alt="" /></a></li>
+      	<li><a href="team.htm">&nbsp;&nbsp;&nbsp;&nbsp;Team</a></li>
+      	<li><a href="plans.htm">&nbsp;&nbsp;&nbsp;&nbsp;Plan</a></li>
+      	<li><a href="legal.htm">&nbsp;&nbsp;&nbsp;&nbsp;Legal</a></li>
+      <li class="separator"><a class="separator" href="users.htm">Users &#160;&#160;
+                            <img src="http://www.eclipse.org/eclipse.org-common/themes/Phoenix/images/leftnav_bullet_down.gif" border="0" alt="" /></a></li>
+      	<li><a href="http://wiki.eclipse.org/index.php/ACTF_Installation">&nbsp;&nbsp;&nbsp;&nbsp;Getting Started</a></li>
+      	<li><a href="http://www.eclipse.org/ACTF/downloads.php">&nbsp;&nbsp;&nbsp;&nbsp;Downloads</a></li>
+        <li><a href="http://www.eclipse.org/ACTF/tools.php">&nbsp;&nbsp;&nbsp;&nbsp;Tools</a></li>
+      <li class="separator"><a class="separator" href="users.htm">Contributors &#160;&#160;
+                            <img src="http://www.eclipse.org/eclipse.org-common/themes/Phoenix/images/leftnav_bullet_down.gif" border="0" alt="" /></a></li>
+      	<li><a href="http://www.eclipse.org/ACTF/contributing.php">&nbsp;&nbsp;&nbsp;&nbsp;Contributing</a></li>
+      	<li><a href="http://dev.eclipse.org/viewcvs/index.cgi/org.eclipse.actf/?root=Technology_Project">&nbsp;&nbsp;&nbsp;&nbsp;CVS Repository</a></li>
+      	<li><a href="documents.htm">&nbsp;&nbsp;&nbsp;&nbsp;Documents</a></li>
+      	<li><a href="conventions.htm">&nbsp;&nbsp;&nbsp;&nbsp;Guidelines</a></li>
+        <li><a href="workitems.htm">&nbsp;&nbsp;&nbsp;&nbsp;Work Items</a></li>
+       <li class="separator"><a class="separator" href="users.htm">Contacts &#160;&#160;
+                            <img src="http://www.eclipse.org/eclipse.org-common/themes/Phoenix/images/leftnav_bullet_down.gif" border="0" alt="" /></a></li>
+      	<li><a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&product=ACTF&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED">
+                            &nbsp;&nbsp;&nbsp;&nbsp;Bug Reports</a></li>
+      	<li><a href="http://www.eclipse.org/newsportal/thread.php?group=eclipse.technology.actf">&nbsp;&nbsp;&nbsp;&nbsp;Newsgroups</a></li>
+      	<li><a href="https://dev.eclipse.org/mailman/listinfo/actf-dev">&nbsp;&nbsp;&nbsp;&nbsp;Mailing Lists</a></li>      	
+      <li style="background-image: url(http://www.eclipse.org/eclipse.org-common/themes/Phoenix/images/leftnav_fade.jpg); 
+                 background-repeat: repeat-x; border-style: none;"><br /><br /><br /><br /><br /><br /><br /></li>
+    </ul>
+</div>
+
+<div id="container">
+   <div id="maincontent">
+
+
+<div id="midcolumn">
 	
-	#
-	# Begin: page-specific settings.  Change these. 
-	$pageTitle 		= "ACTF";
-	$pageKeywords	= "Type, page, keywords, here";
-	$pageAuthor		= "Type your name here";
-	
-	# Add page-specific Nav bars here
-	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
-	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
-	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
-	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
 
-	# End: page-specific settings
-	#
-		
-	# Paste your HTML content between the EOHTML markers!	
-	$html = <<<EOHTML
+<DIV id=midcolumn>
+<H1>Accessibility Tools Framework (ACTF)</H1>
+<P>The Accessibility Tools Framework (ACTF) is an incubation phase project that is a subproject of the Eclipse Technology Project.</P>
+<P>The ACTF will provide both a collection of exemplary accessibility-related tools as well as a set of components for building such tools. The framework will allow users/developers to build and use various types of accessibility tools such as compliance validation tools, assistive technology simulation tools, usability visualization tools, unit-testing tools, and alternative interfaces for people with disabilities. These tools will be integrated into a single tooling environment on top of the Eclipse framework and collaborate with each other and other Eclipse projects to provide a comprehensive development environment for creating accessible applications and content.</P>
+<P>For additional details about the ACTF project, please visit the <A 
+href="http://www.eclipse.org/proposals/actf/">project proposal page</A>. More 
+information will be available shortly. </P>
+<H2>Source code migration</H2>
+<P>The IBM development teams are currently working to move source code and resources to the ACTF Eclipse CVS repository. Obviously, this will be an ongoing process but initial components may appear as early as November-December 2007. </P>
+<H2>Join the project</H2>You can join the project, <A 
+href="https://dev.eclipse.org/mailman/listinfo/actf-dev">subscribing to the 
+mailing-list </A>and start the discussion at <A 
+href="http://www.eclipse.org/newsportal/thread.php?group=eclipse.technology.actf">the 
+newsgroup</A>. We would like to hear from you! </DIV>
+<DIV id=rightcolumn>
+<DIV class=sideitem>
+<H6>Related links</H6>
+<UL>
+  <LI><A href="http://www.eclipse.org/proposals/actf">Project proposal</A> 
+  <LI><A 
+  href="http://www.eclipse.org/proposals/actf/ACTF_CreationReview.pdf">Creation 
+  Review Slides </A></LI></UL></DIV>
+<DIV class=sideitem>
+<H6 align="center">Incubation</H6>
+<DIV align=center><A href="http://www.eclipse.org/projects/gazoo.php"><IMG 
+src="http://www.eclipse.org/images/egg-incubation.png" align=center border=0></A> 
+</DIV></DIV></DIV></DIV></DIV>
 
-	<div id="midcolumn">
-	  This project was just provisioned, so there is nothing to see here.
-	</div>
-
-EOHTML;
 
 
-	# Generate the web page
-	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
-?>
+<div id="footer">
+   <ul id="footernav">
+      <li class="first"><a href="/">Home</a></li>
+      <li><a href="/legal/privacy.php">Privacy Policy</a></li>
+      <li><a href="/legal/termsofuse.php">Terms of Use</a></li>
+      <li><a href="/org/foundation/contact.php">Contact</a></li>
+      <li><a href="/legal/">Legal</a></li>
+      <li class="spacer">&nbsp;</li>
+      <li class="first"><a class="smallText" title="Small Text" href="#" onclick="setActiveStyleSheet('small');return false;">
+                 A</a> <a class="largeText" title="Large Text" href="#" onclick="setActiveStyleSheet('large');return false;">A</a></li>
+   </ul>
+   <p>Copyright &copy; 2007 The Eclipse Foundation. All Rights Reserved</p>
+</div>
+</body>
+</html>
