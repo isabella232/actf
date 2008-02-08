16e5b571ca5667b69175f7dd505e954c83b0e402
kfukuda <kfukuda>
2008-02-08 09:03:32 +0000

    reorder

--- test/projectNav.php
+++ test/projectNav.php
@@ -3,12 +3,12 @@
 	# PHP for Standard Left Menu
 	$Nav->setLinkList( array() );
 	$Nav->addCustomNav( "Information about ACTF", "/projects/project_summary.php?projectid=technology.actf", "", 1  );
-	$Nav->addNavSeparator("ACTF Home", 		"/actf");
-	$Nav->addCustomNav("Developer Resoruces", 	"/actf/contributors.php", "", 1);
-	$Nav->addCustomNav("Downloads", 		"/actf/downloads.php", "", 1);
 	$Nav->addCustomNav("Newsgroups", 	"http://www.eclipse.org/newsportal/thread.php?group=eclipse.technology.actf", "", 1);
 	$Nav->addCustomNav("Bugs",			"https://bugs.eclipse.org/bugs/buglist.cgi?product=ACTF", "", 1);
 	$Nav->addCustomNav("Mailing Lists",	"http://dev.eclipse.org/mhonarc/lists/actf-dev/maillist.html", "", 1);
+	$Nav->addNavSeparator("ACTF Home", 		"/actf");
+	$Nav->addCustomNav("Developer Resoruces", 	"/actf/contributors.php", "", 1);
+	$Nav->addCustomNav("Downloads", 		"/actf/downloads.php", "", 1);
 	$Nav->addCustomNav("Plan", 			"/actf/plan.php", "", 1);
 	$Nav->addCustomNav("Team", 			"/actf/team.php", "", 1);
 	$Nav->addNavSeparator("Project Wiki", 	"http://wiki.eclipse.org/ACTF", "", 1);	
