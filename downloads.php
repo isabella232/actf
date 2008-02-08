cfb2654a304c6ff13d0a1d67854da93a15f49267
kfukuda <kfukuda>
2008-02-08 09:12:14 +0000

    recover standard nav items

--- downloads.php
+++ downloads.php
@@ -18,8 +18,7 @@
 	$pageAuthor		= "ACTF Team";
 	
 	# PHP for Standard Left Menu
-	$Nav->setLinkList( array() );
-$Nav->addCustomNav( "Information about ACTF", "/projects/project_summary.php?projectid=technology.actf", "", 1  );
+	include('projectNav.php');
 
 	# Add page-specific Nav bars here
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
--- projectNav.php
+++ projectNav.php
@@ -2,7 +2,16 @@
 
 	# PHP for Standard Left Menu
 	$Nav->setLinkList( array() );
-$Nav->addCustomNav( "Information about ACTF", "/projects/project_summary.php?projectid=technology.actf", "", 1  );
+	$Nav->addCustomNav( "Information about ACTF", "/projects/project_summary.php?projectid=technology.actf", "", 1  );
+	$Nav->addCustomNav("Newsgroups", 	"http://www.eclipse.org/newsportal/thread.php?group=eclipse.technology.actf", "", 2);
+	$Nav->addCustomNav("Bugs",			"https://bugs.eclipse.org/bugs/buglist.cgi?product=ACTF", "", 2);
+	$Nav->addCustomNav("Mailing Lists",	"http://dev.eclipse.org/mhonarc/lists/actf-dev/maillist.html", "", 2);
+	$Nav->addNavSeparator("ACTF Home", 		"/actf");
+	$Nav->addCustomNav("Developer Resoruces", 	"/actf/contributors.php", "", 1);
+	$Nav->addCustomNav("Downloads", 		"/actf/downloads.php", "", 1);
+	$Nav->addCustomNav("Plan", 			"/actf/plan.php", "", 1);
+	$Nav->addCustomNav("Team", 			"/actf/team.php", "", 1);
+	$Nav->addNavSeparator("Project Wiki", 	"http://wiki.eclipse.org/ACTF", "", 1);	
 
 	# Add page-specific Nav bars here
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
