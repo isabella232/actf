bc7f51665db43e7bce1c9473fd7f7fd46f7c960f
kfukuda <kfukuda>
2008-02-19 12:40:23 +0000

    use metadata for menu items

--- projectNav.php
+++ projectNav.php
@@ -1,11 +1,31 @@
 <?php
+/*******************************************************************************
+ * Copyright (c) 2007 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Michael Squillace - Initial Contribution
+ *    Kentarou FUKUDA - add Menu Items
+ *******************************************************************************/
 
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/actf/common/navUtil.class.php");
+	$navUtil = new NavUtil("technology.actf");
+ 
 	# PHP for Standard Left Menu
 	$Nav->setLinkList( array() );
 	$Nav->addCustomNav( "Information about ACTF", "/projects/project_summary.php?projectid=technology.actf", "", 1  );
-	$Nav->addCustomNav("Downloads", 		"/actf/downloads/index.php", "", 1);
-	$Nav->addCustomNav("Developer Resources", 	"/actf/contributors.php", "", 1);
-	$Nav->addNavSeparator("Project Wiki", 	"http://wiki.eclipse.org/ACTF", "", 1);	
+
+	$Nav->addCustomNav("Developer Resources", 	$navUtil->contributorsurl, "", 1);
+	$Nav->addCustomNav("Downloads",				$navUtil->downloadsurl, "", 1);
+	$Nav->addCustomNav("Project Wiki", 			$navUtil->wikiurl, "", 1);
+	$Nav->addCustomNav("Team", 					"/actf/team.php", "", 1);
+	$Nav->addCustomNav("Mailing Lists", 		$navUtil->mailinglists, "", 1);
+	$Nav->addCustomNav("Bugs", 					$navUtil->bugzilla, "", 1);
+	
+
 
 	# Add page-specific Nav bars here
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
