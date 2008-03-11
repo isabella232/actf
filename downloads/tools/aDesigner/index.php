683bd34b8588a3fbcadd8d23972a6a846dadf228
kfukuda <kfukuda>
2008-03-11 16:57:02 +0000

    new download page

---
+++ downloads/tools/aDesigner/index.php
@@ -0,0 +1,97 @@
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+
+	#*****************************************************************************
+	#
+	# template.php
+	#
+	# Author: 		Denis Roy
+	# Date:			2005-06-16
+	#
+	# Description: Type your page comments here - these are not sent to the browser
+	#
+	#
+	#****************************************************************************
+	
+	# Begin: page-specific settings.  Change these. 
+	$pageTitle 		= "ACTF - aDesigner";
+	$pageKeywords	= "Accessibility Tools Framework, aDesigner, accessibility, ACTF, Eclipse";
+	$pageAuthor		= "Kentarou FUKUDA";
+	
+	include('../../../projectNav.php');
+	include('../../../rightCol.php');
+	include('../../basic_files/buildReleaseTable.php');
+	
+	$releaseTable = generateReleaseTable('tools', 'aDesigner');
+	
+	# End: page-specific settings
+	#
+	
+	#
+	# Paste your HTML content between the EOHTML markers!	
+	$html = <<<EOHTML
+
+<div id="midcolumn">
+	<h1>ACTF aDesigner</h1>
+	<p><ul>
+	<li><a href="#overview">Overview</a></li>
+	<li><a href="#downloads">aDesigner Releases</a></li>
+	<li><a href="#quickStart">Getting started</a></li>
+	<li><a href="#requirements">System Requirements</a></li>
+	</ul></p>
+
+	<h2><a name="overview">Overview</a></h2>
+	<p>aDesigner is a disability simulator that helps designers ensure that 
+	their content and applications are accessible and usable by the visually impaired. 
+	</p>
+	<p>Voice browsers and screen readers read aloud the text on Web pages 
+	and are used by visually impaired people. However, these devices are less 
+	effective with certain kinds of content, such as highly graphical material. 
+	Web developers can use aDesigner to test the accessibility and usability of Web 
+	pages for low-vision and blind people. 
+	</p>
+	<p>
+	aDesigner also helps users to check accessibility of ODF documents and Flash content. 
+	It also has accessibility information (MSAA/IA2) inspection functions. 
+	</p>
+	
+	<h2><a name="downloads">aDesigner Releases</a></h2>
+	$releaseTable
+	
+	<h2><a name="quickStart">Getting started</a></h2>
+	<p>ACTF aDesigner is an Eclipse RCP application. Download and unpack a zip file, 
+	and execute aDesigner.exe in the aDesigner folder. Online help is available from 
+	Help menu.
+	</p>
+	
+	<h2><a name="requirements">System Requirements</a></h2>
+	<ul>
+	 <li>Operating Systems<BR>
+	 <UL>
+	  <LI>Windows&#174; XP or above
+	 </UL>
+  	 <li>Hardware
+	 <UL>
+	  <LI>50MB disk space for installation
+	  <LI>100MB disk space (150MB recommended)
+	  <LI>512MB memory (1GB+ recommended)
+	 </UL>
+	 <LI>Software
+	 <UL>
+	  <li>Java Runtime Environment (JRE) 1.5.0 or above</li>
+	  <li>Internet Explorer 6.0 or above</li>
+	  <li>For Flash Accessibility mode:  Adobe&reg; Flash Player, Version 8 or above</li>
+	  <li>For OpenDocument Accessibility mode:  OpenOffice.org 2.1.0 or above</li>
+  	 </UL>
+	</ul>
+
+</div>
+
+$rightColumn
+
+EOHTML;
+
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
+	
---
+++ downloads/tools/aiBrowser/index.php
@@ -0,0 +1,117 @@
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+
+	#*****************************************************************************
+	#
+	# template.php
+	#
+	# Author: 		Denis Roy
+	# Date:			2005-06-16
+	#
+	# Description: Type your page comments here - these are not sent to the browser
+	#
+	#
+	#****************************************************************************
+	
+	# Begin: page-specific settings.  Change these. 
+	$pageTitle 		= "ACTF - aiBrowser";
+	$pageKeywords	= "Accessibility Tools Framework, aiBrowser, accessibility, ACTF, Eclipse, Accessibility Internet Browser for Multimedia, multimedia";
+	$pageAuthor		= "Kentarou FUKUDA";
+	
+	include('../../../projectNav.php');
+	include('../../../rightCol.php');
+	include('../../basic_files/buildReleaseTable.php');
+	
+	$releaseTable = generateReleaseTable('tools', 'aiBrowser');
+	$releaseTable2 = generateReleaseTable('tools', 'aiBrowser_installer');
+	
+	# End: page-specific settings
+	#
+	
+	#
+	# Paste your HTML content between the EOHTML markers!	
+	$html = <<<EOHTML
+
+<div id="midcolumn">
+	<h1>ACTF Accessibility Internet Browser for Multimedia (aiBrowser)</h1>
+	<p><ul>
+	<li><a href="#overview">Overview</a></li>
+	<li><a href="#downloads">aiBrowser Releases</a></li>
+	<li><a href="#quickStart">Getting started</a></li>
+	<li><a href="#requirements">System Requirements</a></li>
+	</ul></p>
+
+	<h2><a name="overview">Overview</a></h2>
+	<p>The use of multimedia content has increased dramatically over the last 
+	few years, but people with limited or no vision have not been able to fully 
+	enjoy the benefits of these advances. This new multimedia browsing 
+	accessibility tool provides people with visual impairments a level of control 
+	more comparable to a sighted person using a mouse. For example, while enjoying
+	a streaming video, visually impaired people can now select the <b>play</b> 
+	button by simply pressing a predefined shortcut key instead of searching in 
+	the content for buttons that control the video.
+	</p>
+	<p>Users can also adjust the volume of an individual source in order to 
+	identify and listen to different sound sources without losing track of the 
+	screen-reading software because of the sound of a video. If a content creator 
+	wants to provide a voice narrative for a video, he can write a text script as 
+	a piece of metadata; the tool adds the audio descriptions by using text-to-speech 
+	engines.  Future plans for extending this technology include enabling flexible 
+	audio speed control and contributing this work to an open-source development project.  
+	Such contribution will accelerate development and adoption of tools that make 
+	Web-based multimedia content accessible to the visually impaired.
+	<p/>
+	
+	<h2><a name="downloads">aiBrowser Releases</a></h2>
+	$releaseTable2
+
+	$releaseTable
+	
+	<h2><a name="quickStart">Getting started</a></h2>
+	<p>ACTF aiBrowser is an Eclipse RCP application. There are two types of downloads, 
+	installer and zip file. To use aiBrowser with JAWS screen reader, download 
+	an installer version of aiBrowser.
+	</p>
+
+	<h3>Installer</h3>
+	<p>Download and execute an installer. Then, follow the instructions from the 
+	installation wizard. After the installation, you can start aiBrowser from 
+	'All Programs' menu > 'ACTF Accessibility Internet Browser' > 'aiBrowser'. 
+	Online help is available from Help menu.
+	</p>
+
+	<h3>Zip file</h3>
+	<p>Download and unpack a zip file, and execute aiBrowser.exe in the aiBrowser 
+	folder. Online help is available from Help menu.
+	</p>
+		
+	<h2><a name="requirements">System Requirements</a></h2>
+	<ul>
+	 <li>Operating Systems<BR>
+	 <UL>
+	  <LI>Windows&#174; XP SP2 (recommended) or above
+	 </UL>
+  	 <li>Hardware
+	 <UL>
+	  <LI>50MB disk space for installation
+	  <LI>100MB disk space (150MB recommended)
+	  <LI>512MB memory (1GB+ recommended)
+	 </UL>
+	 <LI>Software
+	 <UL>
+	  <li>Java Runtime Environment (JRE) 1.5.0 or above</li>
+	  <li>Internet Explorer 6.0 or above</li>
+	  <li>To use Flash Content:  Adobe&reg; Flash Player, Version 8 or above</li>
+  	 </UL>
+	</ul>
+
+</div>
+
+$rightColumn
+
+EOHTML;
+
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
+	
