11e4c054eb9382884999bb098dcb3172e48fe884
kfukuda <kfukuda>
2008-11-12 18:28:11 +0000

    add documentations

--- docs/index.php
+++ docs/index.php
@@ -31,21 +31,41 @@
 
  <h1>ACTF Documentation</h1>
  <div class="homeitem3col">
-	<h3>Documentation for developers who use ACTF</h3>
+	<h3>Documentation for developers who use/extend ACTF</h3>
 	<ul>       
-       <li><a href="developers/ACTF.pdf">ACTF Getting Started</a></li>
+       <li><div>October 12 2008</div><a href="developers/ACTF.pdf">ACTF Getting Started</a></li>
     </ul>
  </div>
- <!--
  <div class="homeitem3col">
-	<h3>Documentation for users of ACTF and ACTF tools</h3>
-
+	<h3>Documentation for users of ACTF SDKs/runtimes/tools</h3>
+	<p>note: many of ACTF SDKs/runtimes/tools includes online documentation. Please use Help menu to read it.</p>
+	<ul>
+       <li><div>October 08 2008</div>Tool: <a href="users/aDesigner/docs/">ACTF aDesigner (v0.5) Help</a> 
+       (Japanese version is available from <a href="users/aDesigner/ja/docs/">here</a>)
+       </li>
+       <li><div>October 08 2008</div>Tool: <a href="users/aiBrowser/docs/">ACTF aiBrowser (v0.5) Help
+       (Japanese version is available from <a href="users/aiBrowser/ja/docs/">here</a>)
+       </li>
+	</ul>	
  </div>
- -->
  <div class="homeitem3col">
 	<h3>Technical documentation for contributors/committers</h3>
 	<p>Coming soon.</p>
  </div> 
+ <div class="homeitem3col">
+	<h3>Presentations</h3>
+	<ul>
+	   <li><div>March 18 2008</div>
+	   EclipseCon 2008 (short talk) - 
+	   <a href="http://www.eclipsecon.org/2008/sub/attachments/What_is_the_Accessibility_Tools_Framework.pdf">
+	   What is the Accessibility Tools Framework?</a>
+	   <li><div>March 18 2008</div>
+	   EclipseCon 2008 (long talk) - 
+	   <a href="http://www.eclipsecon.org/2008/sub/attachments/ACTF_Visualization_Engines_and_Components_for_Alternative_Interfaces.pdf">
+	   ACTF Visualization Engines and Components for Alternative Interfaces</a>
+	</ul>
+ </div> 
+ 
 </div>
 
 $rightColumn
