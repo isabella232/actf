85e4e7e60cee2a05af7b6f0d4140c3f68668c147
kfukuda <kfukuda>
2010-02-28 17:01:30 +0000

    ACTF restructure

--- downloads/archive/index.php
+++ downloads/archive/index.php
@@ -13,7 +13,7 @@
 	#****************************************************************************
 	
 	# Begin: page-specific settings.  Change these. 
-	$pageTitle 		= "ACTF Archive Downloads";
+	$pageTitle 		= "ACTF Archived Downloads";
 	$pageKeywords	= "Accessibility Tools Framework, accessibility, ACTF, Eclipse";
 	$pageAuthor		= "ACTF Team";
 	
@@ -29,7 +29,7 @@ include('../../projectNav.php');
 
 
 	<div id="midcolumn">
-	<h1>ACTF Archive Downloads</h1>
+	<h1>ACTF Archived Downloads</h1>
 
 <h2 id="release">ACTF Previous Release (0.5)</h2>
 
@@ -44,7 +44,7 @@ include('../../projectNav.php');
 	<a href="http://download.eclipse.org/technology/actf/0.5/update-site/">http://download.eclipse.org/technology/actf/0.5/update-site/</a><br>
 	<b>Other downloads: </b>
 	<a href="http://www.eclipse.org/downloads/download.php?file=/technology/actf/sdk/visualization/0.5.0/actf-visualization-sdk-200810080000-incubation.zip">
-	actf-visualization-sdk-200810080000-incubation.zip<img src="../images/go-bottom.png" alt=""></a>
+	actf-visualization-sdk-200810080000-incubation.zip<img src="../../images/go-bottom.png" alt=""></a>
 	</p>
 	<p>
 	(Please try <a href="tools/aDesigner/index.php">ACTF aDesigner</a>, too. aDesigner is an exemplary tool developed on top of the Validation SDK.)
@@ -58,7 +58,7 @@ include('../../projectNav.php');
 	<a href="http://download.eclipse.org/technology/actf/0.5/update-site/">http://download.eclipse.org/technology/actf/0.5/update-site/</a><br>
 	<b>Other downloads: </b>
 	<a href="http://www.eclipse.org/downloads/download.php?file=/technology/actf/sdk/ai/0.5.0/actf-ai-sdk-200810080000-incubation.zip">
-	actf-ai-sdk-200810080000-incubation.zip<img src="../images/go-bottom.png" alt=""></a>
+	actf-ai-sdk-200810080000-incubation.zip<img src="../../images/go-bottom.png" alt=""></a>
 	</p>
 	<p>
 	(Please try <a href="tools/aiBrowser/index.php">ACTF aiBrowser</a>, too. aiBrowser is an exemplary tool developed on top of the Alternative Interface SDK.)
@@ -70,21 +70,26 @@ include('../../projectNav.php');
 	<hr class="clearer" />	
 	<h2 id="tools">ACTF Validation Components</h2>
 	<div class="homeitem3col">
+	<h3>Overview</h3>
+	<p>
 	ACTF Validation Components are now dormant and have no down stream dependencies or consumers of which we are aware. 
 	ACTF community decided to remove it from the ACTF project (see <A href="http://dev.eclipse.org/mhonarc/lists/actf-dev/msg00286.html">ACTF developers list</a>).
 	The entire source code and artifacts of Validation Components are archived and listed below.
+	</p>
 
 	<h3>Archived Source Files</h3>
+	<p>
 	The last snapshot of ACTF CVS that includes Validation Components is available from here.
+	</p>
 
 	<h3>Java Validation Componentry (Javaco)</h3>
-	<p><a href="components/javaco/index.php">Javaco<img src="../images/go-bottom.png" alt=""></a> is a collection of components that can be leveraged by the Java developer from within the JDT to perform 
+	<p><a href="components/javaco/index.php">Javaco<img src="../../images/go-bottom.png" alt=""></a> is a collection of components that can be leveraged by the Java developer from within the JDT to perform 
 	accessibility compliance validations of Java applications.  These validations are dynamic, occuring as the application is being used so that accessibility 
 	evaluation need not be a distinct part of the testing cycle.  Other components allow the developer to unit-test their GUI-based 
 	applications for accessibility, correcting accessibility deficiencies right in the source code.</p>
 
     <h3>Web Validation Componentry (Webelo)</h3>
-    <p><a href="components/webelo/index.php">Webelo<img src="../images/go-bottom.png" alt=""></a> is a collection of components that can be leveraged by the web content author 
+    <p><a href="components/webelo/index.php">Webelo<img src="../../images/go-bottom.png" alt=""></a> is a collection of components that can be leveraged by the web content author 
     or web appplication developer from within the WTP, JEE, or JDT to perform 
     accessibility compliance validations of web content or applications.  These validations are dynamic, occuring as the application is being used so that accessibility 
     evaluation need not be a distinct part of the testing cycle.</p>
@@ -92,7 +97,7 @@ include('../../projectNav.php');
 	
 	<h3>Accessibility Probe (AccProbe)</h3>
 	
-	<p><a href="tools/accprobe/index.php">Accessibility Probe<img src="../images/go-bottom.png" alt=""></a> is a standalone, Eclipse Rich-Client Product (RCP) application that provides 
+	<p><a href="tools/accprobe/index.php">Accessibility Probe<img src="../../images/go-bottom.png" alt=""></a> is a standalone, Eclipse Rich-Client Product (RCP) application that provides 
 	a view of the accessible object hierarchy of a currently running application or rendered document and of the properties of the accessible objects of that application 
 or document. It can also serve as an event monitor for tracking the events
 fired by these accessible objects. It is meant to combine the functionality of a variety of accessibility testing tools 
