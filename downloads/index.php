91406892a8025a04974713828e94be5d4f914fb2
kfukuda <kfukuda>
2010-09-24 14:42:01 +0000

    ACTF 0.8.1

--- downloads/index.php
+++ downloads/index.php
@@ -52,6 +52,8 @@ include('../projectNav.php');
 			<a href="http://download.eclipse.org/technology/actf/0.8/update-site/">
 			http://download.eclipse.org/technology/actf/0.8/update-site/</a><br>
 			<b>Other downloads: </b>
+			<a href="http://www.eclipse.org/downloads/download.php?file=/technology/actf/sdk/visualization/0.8.1/actf-visualization-0.8.1-win32-incubation.zip">
+			actf-visualization-0.8.1-win32-incubation.zip <img src="../images/go-bottom.png" alt=""></a>
 			<a href="http://www.eclipse.org/downloads/download.php?file=/technology/actf/sdk/visualization/0.8.0/actf-visualization-0.8.0-win32-incubation.zip">
 			actf-visualization-0.8.0-win32-incubation.zip <img src="../images/go-bottom.png" alt=""></a>
 		</p>
