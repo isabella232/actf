2dff6535e0f68981411e1e932cab35c9fc9867b7
kfukuda <kfukuda>
2012-02-20 14:27:41 +0000

    miChecker overview (in ja)

--- downloads/tools/miChecker/index_ja.php
+++ downloads/tools/miChecker/index_ja.php
@@ -55,7 +55,8 @@
     以下のリンクから最新版の付属文書を入手した上でご活用下さい。
 	</p>
 	<p>
-     <a href="worksheet_ja.zip">ワークシート （zip形式： Excel形式およびOpenOffice形式のワークシートが含まれます）</a>
+     <a href="worksheet_ja.zip">ワークシート</a>　（zip形式： Excel形式およびOpenOffice形式のワークシートが含まれます）<br>
+
     <div>全てのダウンロードは 
 	<a href="/legal/epl/notice.php">Eclipse Foundation Software User Agreement</a>の条件の下で提供されます。
 	</div>
