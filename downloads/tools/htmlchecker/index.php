06f7e07b2acb41db784c67542e168cb72dcb9d31
kfukuda <kfukuda>
2012-05-23 07:43:01 +0000

    [337017, 337018]

---
+++ downloads/tools/htmlchecker/index.php
@@ -0,0 +1,55 @@
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+
+	#*****************************************************************************
+	#
+	# template.php
+	#
+	# Author: 		Denis Roy
+	# Date:			2005-06-16
+	#
+	# Description: Type your page comments here - these are not sent to the browser
+	#
+	#
+	#****************************************************************************
+	
+	# Begin: page-specific settings.  Change these. 
+	$pageTitle 		= "ACTF - Example HTML Checker build instruction";
+	$pageKeywords	= "Accessibility Tools Framework, miChecker, accessibility, ACTF, Eclipse";
+	$pageAuthor		= "Kentarou FUKUDA";
+	
+	header("Content-Type: text/html;charset=UTF-8");
+	
+	include('../../../projectNav.php');
+	include('../../../rightCol.php');
+		
+	# End: page-specific settings
+	#
+	
+	#
+	# Paste your HTML content between the EOHTML markers!	
+	$html = <<<EOHTML
+
+<div id="midcolumn">
+	<h1>About HTML Checker</h1>
+	<p>HTML Checker is an example of Web accessibility evaluation application by using ACTF SDK.
+	This example shows how to implement an automated accessibility evaluation function on top of ACTF. 
+	It also includes how to access statistics data of evaluation result.	
+	</p>
+
+	<h1>HTML Checker build instruction</h1>
+	<p>To build an HTML Checker, please follow instructions below.</p>
+	<p><a href="htmlchecker_development.pdf">HTML Checker build instruction (PDF format)</a><br/>
+	<p><a href="htmlchecker_development_ja.pdf">HTML Checker build instruction (PDF format, in Japanese)</a>	
+	</p>
+	
+</div>
+
+$rightColumn
+
+EOHTML;
+
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
+	
