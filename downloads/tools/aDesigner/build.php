7a6ccf0741dbfd7a9d250d16907d0c2fc88f7bfa
kfukuda <kfukuda>
2012-06-27 11:37:06 +0000

    update instruction

--- downloads/tools/aDesigner/build.php
+++ downloads/tools/aDesigner/build.php
@@ -44,10 +44,10 @@
 	 <LI>Software
 	 <UL>
 	  <li><a href="http://www.eclipse.org/downloads/moreinfo/jre.php">Java Runtime Environment (JRE) 5.0 or above</a></li>
-	  <li><a href="http://www.eclipse.org/downloads/">Eclipse SDK 3.5 or above</a></li>
-	  <li>Internet Explorer 6.0 or above</li>
+	  <li><a href="http://archive.eclipse.org/eclipse/downloads/drops/R-3.7.2-201202080800/">Eclipse SDK 3.7.x</a></li>
+	  <li>Internet Explorer 7.0 or above</li>
 	  <li>For Flash Accessibility mode:  Adobe&reg; Flash Player, Version 8 or above</li>
-	  <li>For OpenDocument Accessibility mode:  OpenOffice.org 3.2 or above</li>
+	  <li>For OpenDocument Accessibility mode:  OpenOffice.org 3.2.x</li>
   	 </UL>
 	</ul>
 	</p>
